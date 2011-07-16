7012b12abe1e2535f8b4989a141ab1a1ef909b28
Dmitry Panin <pank.dm@gmail.com>
2011-07-16 15:29:37 +0400

    polishing run script

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -1,24 +1,27 @@
 
 DATA=$1
 SPLITTED="gbk-splitted"
-#SPLITTED="gbk-1"
 MAPPER="genefinder"
 HADOOP=hadoop/bin/hadoop
 
 g++ -O2 splitter.cpp -o splitter
 g++ -O2 genefinder.cpp -o genefinder
 
-cat $DATA |./splitter > data-splitted.txt
+cat $DATA |./splitter | head -n 3 > data-splitted.txt
+
+
+
 
 $HADOOP dfs -rmr $SPLITTED
 $HADOOP fs -put data-splitted.txt $SPLITTED
 
 $HADOOP dfs -rmr gbk
 $HADOOP jar hadoop/contrib/streaming/hadoop-streaming-0.20.203.0.jar \
-    -numReduceTasks 0 \
+    -D mapred.map.tasks=300 \
     -file "$MAPPER" \
     -input "$SPLITTED" \
     -output "gbk" \
     -mapper "./$MAPPER" \
-    -jobconf mapred.map.tasks=1000
+    -reducer org.apache.hadoop.mapred.lib.IdentityReducer \
+    
 $HADOOP jar yssdc/team5.jar gbk/part-00000 tmp result.txt
