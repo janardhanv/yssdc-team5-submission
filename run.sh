06e3636fe62da1710d416ddd1c6473e13c1c6424
Dmitry Panin <pank.dm@gmail.com>
2011-07-17 04:11:27 +0400

    adding status comment to run script

--- run.sh
+++ run.sh
@@ -18,14 +18,17 @@ HADOOP_STREAMING="/usr/lib/hadoop-0.20/contrib/streaming/hadoop-streaming-0.20.2
 
 mkdir -p $WORK
 
+echo "splitting file.."
 cat $DATA | $BINDIR/splitter  > $WORK/data-splitted.txt
 
-
 $HADOOP dfs -rmr $SPLITTED
+
+echo "putting file to HDFS.."
 $HADOOP fs -put $WORK/data-splitted.txt $SPLITTED
 
 $HADOOP dfs -rmr gbk
 $HADOOP dfs -rmr tmp
+
 
 $HADOOP jar $HADOOP_STREAMING \
     -D mapred.map.tasks=1000 \
@@ -34,7 +37,8 @@ $HADOOP jar $HADOOP_STREAMING \
     -output "gbk" \
     -mapper "./$MAPPER" \
     -reducer org.apache.hadoop.mapred.lib.IdentityReducer \
-    
+
+echo "processing results.."    
 $HADOOP jar $ROOTDIR/team5.jar gbk/part-00000 tmp $WORK/result.txt
 cp $WORK/result.txt $OUTPUT
 
